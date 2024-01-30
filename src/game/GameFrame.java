7e28b06f765ee3e226db96157da1c41779292c62
gonzalopetosa <gonzalopetosa1234@gmail.com>
2024-01-30 00:35:03 -0300

    snake game create

---
+++ src/game/GameFrame.java
@@ -0,0 +1,17 @@
+package game;
+import javax.swing.JFrame;
+public class GameFrame extends JFrame{
+
+    GameFrame(){
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("SnakeGame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+
+}
---
+++ src/game/SnakeGame.java
@@ -0,0 +1,10 @@
+package game;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+
+    }
+}
